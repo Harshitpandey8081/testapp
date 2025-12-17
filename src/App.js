ae69faf708d5d4ee36b80ede9466a24e2c82b257
Harshitpandey8081 <panditharshit1@gmail.com>
2025-12-17 20:23:30 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:7226/api/Home")
+      .then(response => response.json())
+      .then(data => setClients(data))
+      .catch(error => console.error("Error:", error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        This Is Test
-      </header>
+    <div>
+      <h2>Client List</h2>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Client No</th>
+            <th>Client Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clients.map(client => (
+            <tr key={client.clientNo}>
+              <td>{client.clientNo}</td>
+              <td>{client.clientName}</td>
+              <td>{client.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
+    
   );
 }
 
